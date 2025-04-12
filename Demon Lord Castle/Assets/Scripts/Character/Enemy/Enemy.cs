4bf6a8919c514dde7a3b4dc150eb3aea3f55b7b6
PranavAlla <Pranavalla@gmail.com>
2025-04-12 12:11:17 -0400

    Updated Enemy Script
    Edited enemy script to move towards player and detect nearby enemies for a work in progress avoidance mechanic.  Currently commented out until it is working.

--- Demon Lord Castle/Assets/Scripts/Character/Enemy/Enemy.cs
+++ Demon Lord Castle/Assets/Scripts/Character/Enemy/Enemy.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float health;
+    private GameObject player;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
         if (health <= 0)
             Destroy(gameObject);
+
+        // Move towards player
+        //Vector3 targetPos = Vector3.Slerp(rb.transform.position, player.transform.position, 1f * Time.deltaTime);
+        //transform.position = new Vector3(targetPos.x, transform.position.y, targetPos.z);
+
+        // Detect nearby enemies for avoidance
+        //LayerMask enemies = LayerMask.GetMask("Enemy");
+        //Collider[] hitColliders = Physics.OverlapSphere(transform.position, 2f, enemies);
+        
+
     }
 }
