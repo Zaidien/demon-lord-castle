25806aac74cf82b620fb09efe4babe0a46cf8b6d
PranavAlla <Pranavalla@gmail.com>
2025-04-16 12:56:23 -0400

    Enemy Attack logic
    added basic logic for when the enemy attacks and updated prefabs for the enemy

--- Demon Lord Castle/Assets/Scripts/Character/Enemy/Enemy.cs
+++ Demon Lord Castle/Assets/Scripts/Character/Enemy/Enemy.cs
@@ -7,11 +7,15 @@ public class Enemy : MonoBehaviour
     public float health;
     private GameObject player;
     private Rigidbody rb;
+    private bool attackRange;
+    private bool attack;
 
     private void Awake()
     {
         player = GameObject.Find("Player");
         rb = GetComponent<Rigidbody>();
+        attackRange = false;
+        attack = false;
     }
 
     private void Update()
@@ -19,9 +23,20 @@ public class Enemy : MonoBehaviour
         if (health <= 0)
             Destroy(gameObject);
 
+        // Look at Player and detect player in proximity
+        transform.LookAt(player.transform.position);
+        LayerMask playerLayer = LayerMask.GetMask("Player");
+        Collider[] playerHitColliders = Physics.OverlapSphere(transform.position, 1f, playerLayer);
+        if (playerHitColliders.Length > 0)
+            attackRange = true;
+
         // Move towards player
-        Vector3 targetPos = Vector3.Slerp(rb.transform.position, player.transform.position, 1f * Time.deltaTime);
-        transform.position = new Vector3(targetPos.x, transform.position.y, targetPos.z);
+        if (attackRange == false)
+        {
+            Vector3 targetPos = Vector3.Slerp(rb.transform.position, player.transform.position, 1f * Time.deltaTime);
+            transform.position = new Vector3(targetPos.x, transform.position.y, targetPos.z);
+            attack = true;
+        }
 
         // Detect nearby enemies for avoidance
         //LayerMask enemies = LayerMask.GetMask("Enemy");
